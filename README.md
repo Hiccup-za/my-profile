2e263e4b2c6951b330cd12168e1e1cf29b3d821f
Christopher Zeuch <37897176+Hiccup-za@users.noreply.github.com>
2024-07-31 17:46:46 +0200

    Feat: Added Content (#1)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# my-profile
+# My Profile
+
+[Click here to view my profile.](https://my-profile-plum.vercel.app/)
+
+Built using HTML, CSS, and JavaScript.  
+Assisted by ✨Cursor.
---
+++ strings.js
@@ -0,0 +1,45 @@
+const strings = {
+    header: "Hello 👋 I'm Christopher, a QA Engineer from South Africa 🇿🇦",
+
+    aboutMe1: "I'm a hardworking and highly autonomous individual with deep passion for Quality Assurance and Engineering 💪",
+    aboutMe2: "I've worked for various companies and startups; with products ranging from PoCs and MVPs to large production systems in fintech and financial forecasting 💫",
+    aboutMe3: "I can work solo or as part of a team, both are enjoyable. I've also had the privilege of leading a small QA team that worked on various startups, where I gained experience in leading and mentoring 🤝",
+    aboutMe4: "Having spent the last few years working exclusively with startups; my go-to \"style\" is lean and lightweight, enabling me to adjust test strategies as the business grows 🚀",
+
+    kohortexp: "<ul><li>Founding QA Engineer · Apr 2023 - Jul 2024</li></ul>",
+    kohort1: "This was my first opportunity to work as a solo, Founding QA Engineer. My main responsibility was to test all incoming Web App changes from the Platform team.",
+    kohort2: "This meant building, implementing and integrating a testing process that was lean and lightweight, but robust enough to expand as the business grew.",
+    kohort3: "During my time here I implemented an automated scripting strategy using SeleniumBase and a Streamlit app that generated testing datasets.",
+    kohort4: "I also built a Streamlit app for my SeleniumBase scripts where I could spin up and tear down product functionality in any of our environments. This was later improved and productionized as part of a team hackathon.",
+    kohortreason: "<span style='text-decoration: underline;'>Reason for leaving:</span> Retrenchment",
+
+    deltaexp: "<ul><li>Lead QA Engineer · Apr 2022 - Apr 2023</li><li>Senior QA Engineer · May 2021 - March 2022</li></ul>",
+    delta1: "At The Delta I grew from a Senior QA Engineer to a Lead QA Engineer.",
+    delta2: "Upon joining I started working for two Startups, one of which was Precium. Depending on the needs of the business I sometimes worked on other startups for short periods.",
+    delta3: "During this time I was responsible for leading and managing a small team of 3 QA Engineers of varying experience while they worked on their assigned projects.",
+    deltareason: "<span style='text-decoration: underline;'>Reason for leaving:</span> Had the opportunity to work for Kohort",
+
+    entersektexp: "<ul><li>Senior Quality Engineer · Mar 2021 - Apr 2021</li><li>Senior Quality Engineering Analyst · Dec 2019 - Feb 2021</li><li>Quality Engineering Analyst · Jun 2017 - Nov 2019</li><li>Quality Assurance Analyst · Feb 2016 - May 2017</li></ul>",
+    entersekt1: "At Entersekt I grew from a Junior QA Engineer to a Senior QA Engineer, working with almost all of the internal teams.",
+    entersekt2: "I first started in the SDK and Mobile Apps team where I was responsible for planning & executing feature testing, integrating testing & regression cycles.",
+    entersekt3: "I then moved to our Backend team, responsible for deploying and maintaining our backend systems on physical hardware appliances.",
+    entersekt4: "I then moved to our Platform team where I was responsible for organising & co-ordinating large-scale regressions across the organisation. During this time I also worked closely with the DevOps team as a scrum master.",
+    entersektreason: "<span style='text-decoration: underline;'>Reason for leaving:</span> Had the opportunity to work in the Startup space",
+
+    dvtexp: "<ul><li>Junior SQA Analyst · Jun 2015 - Jan 2016</li></ul>",
+    dvt1: "At DVT I learned the fundamentals of Quality Assurance by working on three client projects:",
+    dvt2: "<ul><li>Mobile application on Android & iOS</li><li>Power BI project</li><li>Educational game on Windows</li></ul>",
+    dvt3: "After working on these client projects, I was contracted out to Entersekt where I worked directly with an engineering team for the first time.",
+    dvtreason: "<span style='text-decoration: underline;'>Reason for leaving:</span> Entersekt contract turned into full-time employment",
+
+    techstack: "<ul><li>🎟️ ClickUp</li><li>✨ Cursor with gpt-4o</li><li>⌨️ iTerm2</li><li>🐙 GitKraken</li><li>🌻 Loom</li><li>🐍 Python</li><li>🐉 SeleniumBase</li><li>🔭 Streamlit</li></ul>",
+    technicalexperience: "<ul><li>API testing and automation</li><li>Bare-metal server & applications testing</li><li>Manual & Automated test suite creation & maintenance</li><li>Process roll-out and improvements</li><li>Release planning and co-ordination</li><li>Test plan creation, execution and reporting</li><li>TestOps</li><li>Testing environment setup & maintenance</li><li>Web application testing & automation</li></ul>",
+
+    sideproject1p: "A collection of apps that I built to become familiar with Streamlit.",
+    sideproject1list: "<ul><li>🎛️ Config Selector</li><li>💫 Dashboard</li><li>🪄 Generate Test Data</li></ul>",
+
+    sideproject2p: "A Streamlit app that contains the questions and answers for the AFK Journey Trivia Quiz.",
+    sideproject2list: "<ul><li>💬 Users can type the question (minimum of two words) and click the submit button to get an answer.</li><li>↕️ Alternatively, users can scroll through the True or False tables to find their question.</li></ul>",
+
+    footer: "&copy; 2024 Christopher Zeuch. All rights reserved."
+};
